3abafcc98143042bf60ddd20dcd338e775cd113e
eminbasbayan <emin.basbayan@icloud.com>
2024-03-23 17:16:05 +0300

    c# veri tipleri (data types)

---
+++ 02_veri_tipleri.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1- Tamsayılar (Integers)
+            //int number1 = 10;
+            //int number2 = 20;
+            //Console.WriteLine(number1 + number2);
+            //int x = 2147483647;
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(int.MinValue);
+
+            //2- Kayan Noktalı Sayılar (Floats)
+            //float y = 4.5f;
+            //Console.WriteLine(y);
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(float.MinValue);
+
+            //3- Çift Kayan Noktalı Sayılar (Doubles)
+            //double z = 10.5;
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine(double.MinValue);
+
+            //4- Uzun Tamsayılar (Longs) ve Decimal
+            //long x = 21474836478;
+            //Console.WriteLine(int.MaxValue);
+            //Console.WriteLine(long.MaxValue);
+            //Console.WriteLine(long.MinValue);
+
+            //5- Karakterler ve Dizgiler
+            //string ad = "Emin ";
+            //string soyad = "Başbayan";
+            //Console.WriteLine(ad + soyad);
+            //char karakter = 'e';
+            //Console.WriteLine(karakter);
+
+            //6- var ve dynamic Anahtar Kelimeleri
+            //var a = 5;
+            //a = 2;
+            //a = "8";
+            //dynamic b = "BilGen Yazılım Akademi";
+            //b = 13123;
+
+            //7- Sabitler (Constants) ile const Kullanımı
+            //const int a = 5;
+            //a = 10;
+
+            //8- bool Kullanımı
+            //bool dogru = true;
+            //bool yanlis = false;
+
+            //Console.WriteLine(dogru);
+            //Console.WriteLine(yanlis);
+
+        }
+    }
+}
