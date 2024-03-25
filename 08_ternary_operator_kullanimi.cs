8a4eb1422a1f1dd18aa969c34d92fadb81dc0d09
eminbasbayan <emin.basbayan@icloud.com>
2024-03-25 13:14:21 +0300

    üçlü (ternary) operatör kullanımı

---
+++ 08_ternary_operator_kullanimi.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yas = 20;
+            var yuzebilirMi = yas > 10 ? "Evet" : "Hayır";
+            Console.WriteLine(yuzebilirMi);
+        }
+    }
+}
