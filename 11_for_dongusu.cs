bccb5068fa20f683c12d84f7d816e5ab891bb139
eminbasbayan <emin.basbayan@icloud.com>
2024-04-12 14:14:35 +0300

    for döngüsü

---
+++ 11_for_dongusu.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int k = 0;
+            //while (k < 10) {
+            //    Console.WriteLine("BilGen Yazılım Akademi " + k);
+            //    k++;
+            //}
+
+            for(int i = 0; i < 10; i++) {
+                Console.WriteLine("BilGen Yazılım Akademi " + i);
+            }
+        }
+    }
+}
