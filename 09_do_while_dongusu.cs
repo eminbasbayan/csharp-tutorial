03189c6aef7e1b92b06c792f073ad469469e54c2
eminbasbayan <emin.basbayan@icloud.com>
2024-04-12 13:50:36 +0300

    döngüler (loops) ve do while döngüsü

---
+++ 09_do_while_dongusu.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int k = 0;
+            do {
+                Console.WriteLine("BilGen Yazılım Akademi " + k);
+                k++;
+            } while (k < 10);
+        }
+    }
+}
